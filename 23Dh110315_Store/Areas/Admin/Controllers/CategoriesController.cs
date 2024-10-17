4bd9048024bc751871945ec04292eedbe15d5bde
Trần Thị Thanh Bình <23DH110315@st.huflit.edu.vn>
2024-10-17 23:01:19 +0700

    day len

--- 23Dh110315_Store/Areas/Admin/Controllers/CategoriesController.cs
+++ 23Dh110315_Store/Areas/Admin/Controllers/CategoriesController.cs
@@ -14,75 +14,40 @@ namespace _23Dh110315_Store.Areas.Admin.Controllers
     {
         private StoreEntities1 db = new StoreEntities1();
 
-        // GET: Admin/Categories
-        //GET: lấy dữ liệu từ bảng Category trong DB để hiển thị lên
+        // GET: Categories
+        // GET Lấy dữ liệu từ bảng Category trong DB để hiên thị lên 
         public ActionResult Index()
         {
             return View(db.Categories.ToList());
         }
 
         // GET: Admin/Categories/Details/5
-        //Details: lấy chi tiết một bản ghi có CategoryID = id 
+
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest); //mã lỗi 400: thiếu giá trị truyền vào  
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest); // mã lỗi 400 : thiếu gia trị truyền vào  
             }
             Category category = db.Categories.Find(id);
-            if (category == null) //Không tìm thấy bản ghi 
+            if (category == null) // không tìm thấy bản ghi 
             {
-                return HttpNotFound(); //nã lỗi 404 
+                return HttpNotFound(); // mã lỗi 404 
             }
             return View(category);
         }
 
-        // GET: Admin/Categories/Create
-        // Load from Create 
-        //[HttpGet] là phương thức mặc định, nên không cần khai báo từ khóa
+        // GET: Categories/Create
+        // Load form Create 
+        //[HttpGet] là phương thức mặc đinh , nên không cần khai báo từ khóa  
 
         public ActionResult Create()
         {
             return View();
         }
 
-
-        
-
-        // GET: Admin/Categories/Edit/5
-        //GET: lấy dữ liệu của một danh mục đã có sao cho CategoryID= id 
-        public ActionResult Edit(int? id)
-        {
-            Details(id);
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Category category = db.Categories.Find(id);
-        //    if (category == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(category);
-        }
-
-        // POST: Admin/Categories/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "CategoryID,CategoryName")] Category category)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(category).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(category);
-        }
-        // POST: Admin/Categories/Create
-        //POST: lưu dữ liệu nhập vào từ from Create vào DB 
+        // POST: Categories/Create
+        // POST: Lưu dữ liệu nhập vào từ form Create  vào DB 
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
@@ -99,7 +64,39 @@ namespace _23Dh110315_Store.Areas.Admin.Controllers
             return View(category);
         }
 
-        // GET: Admin/Categories/Delete/5
+        // GET: Categories/Edit/5
+        // GET : Lấy dữ liệu từ danh mục đđã có sao cho CategoryID = id 
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Categories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CategoryID,CategoryName")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(category).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -114,7 +111,7 @@ namespace _23Dh110315_Store.Areas.Admin.Controllers
             return View(category);
         }
 
-        // POST: Admin/Categories/Delete/5
+        // POST: Categories/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
--- 23Dh110315_Store/Areas/Admin/Controllers/ProductsController.cs
+++ 23Dh110315_Store/Areas/Admin/Controllers/ProductsController.cs
@@ -10,123 +10,15 @@ using _23Dh110315_Store.Models;
 
 namespace _23Dh110315_Store.Areas.Admin.Controllers
 {
-    public class ProductsController : Controller
-    {
-        private StoreEntities1 db = new StoreEntities1();
 
-        // GET: Admin/Products
-        public ActionResult Index()
+        public class ProductsController : Controller
         {
-            var products = db.Products.Include(p => p.Category);
-            return View(products.ToList());
-        }
-
-        // GET: Admin/Products/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
+            // GET: Admin/Products
+            public ActionResult Index()
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                return View();
             }
-            Product product = db.Products.Find(id);
-            if (product == null)
-            {
-                return HttpNotFound();
-            }
-            return View(product);
-        }
-
-        // GET: Admin/Products/Create
-        public ActionResult Create()
-        {
-            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName");
-            return View();
-        }
-
-        // POST: Admin/Products/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ProductID,CategoryID,ProductName,ProductPrice,ProductImage,ProductDescription")] Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Products.Add(product);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
-            return View(product);
-        }
-
-        // GET: Admin/Products/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Product product = db.Products.Find(id);
-            if (product == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
-            return View(product);
-        }
-
-        // POST: Admin/Products/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ProductID,CategoryID,ProductName,ProductPrice,ProductImage,ProductDescription")] Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(product).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
-            return View(product);
-        }
-
-        // GET: Admin/Products/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Product product = db.Products.Find(id);
-            if (product == null)
-            {
-                return HttpNotFound();
-            }
-            return View(product);
-        }
-
-        // POST: Admin/Products/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Product product = db.Products.Find(id);
-            db.Products.Remove(product);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
         }
     }
-}
+
+
